7200870f9ec5d789d4dcb79111c3f25aef001c6b
Karinda <karinasirokova5442@gmail.com>
2023-03-25 07:56:07 +0300

    last changes

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int price = 13868;
         int mile = 20;
-        int bonus = price/mile;
+        int bonus = price / mile;
 
         System.out.println("Колличество начисленных миль:" + bonus);
     }
