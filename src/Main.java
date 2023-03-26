17897f8ae9044f1c7d9b4d10c456e32e43738850
Karinda <karinasirokova5442@gmail.com>
2023-03-26 11:38:28 +0300

    sixth commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 13868;
-        int mile = 20;
-        int bonus = price / mile;
-
-        System.out.println("Колличество начисленных миль:" + bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
